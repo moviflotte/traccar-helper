7f5cedd770776ce82e2ec71c9c86411bc665f172
moviflotte <136280595+moviflotte@users.noreply.github.com>
2024-01-08 18:49:18 +0000

    better error log

--- pages/index.vue
+++ pages/index.vue
@@ -190,7 +190,7 @@ export default {
             } catch (e) {
               console.error(e)
               this.error++
-              this.lastError += `${(e.response && e.response.data) || e.message}: ${fields}\n`
+              this.lastError += `${fields}\n${(e.response && e.response.data) || e.message}\n`
             }
           }
         }
