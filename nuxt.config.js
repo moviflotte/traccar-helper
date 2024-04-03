d0fc1b1978686f388481fe0f6010892d625a5af6
moviflotte <136280595+moviflotte@users.noreply.github.com>
2024-04-03 17:06:59 +0100

    import

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Moviflotte Backoffice ' + process.env.npm_package_version,
+    title: 'Import ' + process.env.npm_package_version,
     htmlAttrs: {
       lang: 'en'
     },
